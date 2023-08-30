9d2751616397b08b02cc2fa0bb366b4c3b17d3b9
Your Name <codreanu.vasile.gh@gmail.com>
2023-08-30 17:53:00 +0300

    Intitialized an empty Vaadin project. Closes #1

---
+++ src/main/java/com/cedacri/vaadinlogistics/VaadinLogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.cedacri.vaadinlogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinLogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinLogisticsApplication.class, args);
+	}
+
+}
